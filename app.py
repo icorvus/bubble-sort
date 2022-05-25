04749231b1c782f2c91d629a09034f4321a814ef
icorvus <business.wrona@gmail.com>
2022-05-26 01:25:54 +0200

    Optimized the algorythm

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ def bubble_sort(list):
     k = 1
     while not is_sorted:
         is_sorted = True
-        for i in range(len(list) - 1):
+        for i in range(len(list) - k):
             if list[i] > list[i + 1]:
                 temp = list[i + 1]
                 list[i + 1] = list[i]
                 list[i] = temp
                 is_sorted = False
-        
-            # k += 1
+        k += 1
     return list
