050a4ba603cf345206a370171724df9fe9a3f1b8
icorvus <business.wrona@gmail.com>
2022-05-26 01:09:56 +0200

    Fixed the algorythm

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ def bubble_sort(list):
     is_sorted = False
     k = 1
     while not is_sorted:
-        for i in range(len(list) - k):
-            is_sorted = True
+        is_sorted = True
+        for i in range(len(list) - 1):
             if list[i] > list[i + 1]:
                 temp = list[i + 1]
                 list[i + 1] = list[i]
                 list[i] = temp
                 is_sorted = False
-        k += 1
+        
+            # k += 1
     return list
--- test.py
+++ test.py
@@ -3,15 +3,14 @@ import random
 
 
 def main():
-    list1 = random.sample(range(1, 100), 10)
-    list2 = random.sample(range(1, 2523), 123)
-    list3 = random.sample(range(42, 1733), 2)
-    list4 = random.sample(range(1, 1000000), 9999)
+    list1 = random.sample(range(10), 10)
+    list2 = random.sample(range(1, 20), 8)
+    list3 = random.sample(range(1, 50), 10)
+    list4 = random.sample(range(1, 1000000), 10)
     list5 = random.sample(range(900000, 10000000), 20)
     list6 = random.sample(range(20), 20)
 
     lists = [list1, list2, list3, list4, list5, list6]
-
     passed = True
     for list in lists:
         if bubble_sort(list) != sorted(list):
